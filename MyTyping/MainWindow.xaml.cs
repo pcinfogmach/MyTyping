028d171e3e4ffcb91d6138c1b9afe2fe932c18c2
Admin <Admin@LAPTOP-4HRM131V>
2024-12-19 23:25:36 +0200

    Add project files.

---
+++ MyTyping/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyTyping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string keymap;
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Oem2) // Key.Oem2 represents the '/' key on most keyboards
+            {
+                WriteKeymapToFile();
+            }
+            else
+            {
+                keymap += "\r\n" + e.Key.ToString();
+            }
+        }
+
+        private void WriteKeymapToFile()
+        {
+            string desktopPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "keymap.txt");
+            File.WriteAllText(desktopPath, keymap);
+        }
+    }
+
+}
